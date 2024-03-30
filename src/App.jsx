a2439b3939368fc83fdb83fcd942cfd29a0bcf8d
mdasif8393 <md.asif.8393@gmail.com>
2024-03-30 18:01:13 +0600

    4.7 Synchronizing with Effects - Overview of Effects vs Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Counter from "./components/Counter";
+import ToDo from "./components/ToDo";
 
 function App() {
   return (
     <>
-      <Counter />
+      <ToDo />
     </>
   );
 }
